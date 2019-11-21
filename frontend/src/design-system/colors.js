f2e47b6879f5bc616e2c398761f36893250d5ad0
Bartek <bartek.wieckowski@gmail.com>
2019-11-21 21:53:35 +0100

    add colors, shadows to design system add font, initial styles update prop-types

--- frontend/src/design-system/colors.js
+++ frontend/src/design-system/colors.js
@@ -1,3 +1,18 @@
 export const colors = {
-    red: "pink",
+    apartment: '#F02C6E',
+    room: '#CC4B93',
+    flat: '#A946BE',
+    garage: '#35375A',
+
+    button: '#76DDF3',
+    buttonHover: '#4DD2F0',
+
+    dark: '#3D3A3A',
+    darker: '#242424',
+    black: '#000',
+
+    gray: '#9B9B9B',
+    lighterGray: '#BDBDBD',
+    lightestGray: '#DADADA',
+    white: '#fff'
 };
---
+++ frontend/src/design-system/shadows.js
@@ -0,0 +1,3 @@
+export const shadows = {
+    black01:  '0px 2px 4px rgba(0, 0, 0, 0.1)',
+};
