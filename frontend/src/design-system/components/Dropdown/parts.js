5d78038e0ff036e54af08e8506bed580f77e06c6
Bartek <bartek.wieckowski@gmail.com>
2019-12-18 23:04:38 +0100

    add styled custom Dropdown

---
+++ frontend/src/design-system/components/Dropdown/parts.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import {colors} from "../../";
+
+
+export const RotateContainer = styled.div`
+    transition: transform 0.2s;
+    transform: ${({isOpen}) => isOpen ? 'rotate(180deg)' : 'rotate(0)'};
+`;
+
+export const Container = styled.div`
+    border: solid 1px ${colors.lightestGray};
+    background-color: ${colors.white};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`;
+export const Wrapper = styled.div`
+  height: 31px;
+  width: 100%;
+`;
+export const Selected = styled.p`
+  margin-left: 10px;
+  display: inline;
+  text-align: left;
+`;
+
+export const Button = styled.button`
+  height: 30px;
+  width: 30px;
+  border:none;
+  border-left: solid 1px ${colors.lightestGray};
+  border-bottom: solid 1px ${colors.lightestGray};
+  outline: none;
+`;
+
+export const List = styled.ul`
+  display: ${({isOpen}) => isOpen ? 'block' : 'none'};
+  max-height: 150px;
+  overflow: scroll;
+  border: 1px solid ${colors.lightestGray};
+  border-top: none; 
+  z-index: 5;
+`;
+
+export const ListItem = styled.li`
+  padding: 5px 0;
+  &:hover{
+    background-color: ${colors.lightestGray};
+  }
+`;
