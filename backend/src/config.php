55f157f6efd93d26e0d1c3491df43b0efcc58bef
Bartek <bartek.wieckowski@gmail.com>
2019-12-28 09:42:02 +0100

    config test

---
+++ backend/src/config.php
@@ -0,0 +1,6 @@
+<?php
+    $DB_PORT = '3306';
+    $DB_HOST = 's23.hekko.net.pl';
+    $DB_USER = 'bartex25_myawsomeflkat';
+    $DB_PASSWORD = 'zupvud-nyqbeh-0dedkY';
+    $DB_DATABASE = 'bartex25_myawsomeflkat';
