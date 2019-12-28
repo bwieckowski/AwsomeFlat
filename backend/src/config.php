335f2fef83e6fd43dfb7c10bedcd078ca7dfe962
bwieckowski <bartek.wieckowski@gmail.com>
2019-12-28 09:44:01 +0100

    Delete config.php
    delete config

--- backend/src/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $DB_PORT = '3306';
-    $DB_HOST = 's23.hekko.net.pl';
-    $DB_USER = 'bartex25_myawsomeflkat';
-    $DB_PASSWORD = 'zupvud-nyqbeh-0dedkY';
-    $DB_DATABASE = 'bartex25_myawsomeflkat';
