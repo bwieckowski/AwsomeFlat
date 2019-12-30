483337d739674e70c576f9a7edb1b9361c3711f4
Bartek <bartek.wieckowski@gmail.com>
2019-12-30 21:27:26 +0100

    Replace table model sql with Database backup

--- backend/mockData/Advertisement.sql
+++
@@ -1,5 +0,0 @@
-insert into Advertisement (id_property_type, area, added_time, id_user, id_localization, id_price) values (1, 22, '2019-01-17T20:30:05Z', 1, 1, 1);
-insert into Advertisement (id_property_type, area, added_time, id_user, id_localization, id_price) values (4, 212, '2018-12-27T23:40:17Z', 2, 2, 2);
-insert into Advertisement (id_property_type, area, added_time, id_user, id_localization, id_price) values (2, 183, '2019-04-11T08:44:53Z', 3, 3, 3);
-insert into Advertisement (id_property_type, area, added_time, id_user, id_localization, id_price) values (3, 122, '2019-11-25T13:41:29Z', 4, 4, 4);
-insert into Advertisement (id_property_type, area, added_time, id_user, id_localization, id_price) values (3, 246, '2019-12-01T12:30:05Z', 5, 5, 5);
--- backend/mockData/Facilities.sql
+++
@@ -1,5 +0,0 @@
-insert into Facilities (name) values ('wifi');
-insert into Facilities (name) values ('telewizor');
-insert into Facilities (name) values ('prywatna łazienka');
-insert into Facilities (name) values ('balkon');
-insert into Facilities (name) values ('parking');
