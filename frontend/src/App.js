a69fe87bc18a6da8a4b79dd6aae84bb2c4c6bc85
Bartek <bartek.wieckowski@gmail.com>
2020-01-01 22:33:38 +0100

    add router and views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,35 @@
 import React from 'react';
-import styled from 'styled-components';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom'
+import * as V from 'views';
 import './App.css';
-import Input from "design-system/components/Input/Input";
-import RangeInput from "design-system/components/RangeInput/RangeInput";
-import Dropdown from "design-system/components/Dropdown";
-import Toggle from "design-system/components/Toggle/Toggle";
-import Filter from "modules/Filter/Filter";
-import {colors} from 'design-system'
 
-const Wrapper = styled.div`
-   width: 400px;
-   padding: 10px;
-   background-color: ${colors.background};
-`;
-
-const StyledInput = styled(Input)`
-  margin: 10px 0;
-`;
-
-const StyledRangeInput = styled(RangeInput)`
-  margin: 10px 0;
-`;
-
-const StyledDropdown = styled(Dropdown)`
-  width: 150px;
-`;
-
-const StyledToggle = styled(Toggle)`
-  margin: 10px 0;
-`;
-
-const dropDownCallback = (item) => {
-    console.log(item.target.innerHTML);
-};
+const routes = [
+    {
+       path: '/',
+       component: V.MapPage,
+    },
+    {
+        path: '/playground',
+        component: V.Playground,
+    },
+];
 
 function App() {
   return (
     <div className="App">
-        <Wrapper>
-
-         <StyledInput placeholder={"imie"} type={"text"} movingPlaceholder={false}/>
-         <StyledInput placeholder={"imie"} type={"text"}/>
-         <StyledInput placeholder={"imie"} type={"text"}  initValue={"testowe dane"}/>
-         <StyledInput placeholder={"piniąszki"} type={"number"} unit={"zł"}/>
-         <StyledRangeInput unit={"zł"} />
-         <StyledDropdown onChange={dropDownCallback} optionList={['item1', 'item2', 'item3']} />
-         <StyledToggle onChange={(state) => { console.log(state)}}/>
-         <Filter/>
-        </Wrapper>
+        <Router>
+            {routes.map((route) => (
+                <Route
+                    key={route.path}
+                    path={route.path}
+                    component={route.component}
+                    exact={true}
+                />
+            ))}
+        </Router>
     </div>
   );
 }
---
+++ frontend/src/views/MapPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MapPage = () => {
+    return (
+        <>
+            <h2>MapPage</h2>
+        </>
+    )
+};
+
+export default MapPage;
